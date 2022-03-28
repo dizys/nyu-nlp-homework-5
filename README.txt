8adeb3838b0fae03de910124479031e578713391
Dizy <dev@dizy.cc>
2022-03-28 05:27:30 -0400

    Init project

---
+++ README.txt
@@ -0,0 +1,9 @@
+NYU NLP Homework 5: Feature selection for a Noun Group tagger
+    by Ziyang Zeng (zz2960)
+    Spring 2022
+
+Pre-requisites:
+    - Python 3.8+
+
+Install dependencies:
+    `pip3 install -r requirements.txt`
---
+++ src/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+NYU NLP Homework 5: Feature selection for a Noun Group tagger.
+    by Ziyang Zeng (zz2960)
+    Spring 2022
+"""
+
+
+def main():
+    print("Hello, world!")
+
+
+if __name__ == '__main__':
+    main()
